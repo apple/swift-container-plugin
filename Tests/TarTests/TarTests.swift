84326641bfe7cc3b2dc3665d415fdcd35aaca832
Euan Harris <euanh@apple.com>
2024-12-19 14:45:08 +0000

    testUInt8writeString

--- Tests/TarTests/TarTests.swift
+++ Tests/TarTests/TarTests.swift
@@ -42,4 +42,37 @@ let trailerLen = 2 * blocksize
     func testOctal11(input: Int, expected: String) async throws {
         #expect(octal11(input) == expected)
     }
+
+    @Test func testUInt8writeString() async throws {
+        // Fill the buffer with 0xFF to show null termination
+        var hdr = [UInt8](repeating: 255, count: 21)
+
+        // The typechecker timed out when these test cases were passed as arguments, in the style of the octal tests
+        hdr.writeString("abc", inField: 0..<5, withTermination: .none)
+        #expect(
+            hdr == [
+                97, 98, 99, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            ]
+        )
+
+        hdr.writeString("def", inField: 3..<7, withTermination: .null)
+        #expect(
+            hdr == [97, 98, 99, 100, 101, 102, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]
+        )
+
+        hdr.writeString("ghi", inField: 7..<11, withTermination: .space)
+        #expect(
+            hdr == [97, 98, 99, 100, 101, 102, 0, 103, 104, 105, 32, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]
+        )
+
+        hdr.writeString("jkl", inField: 11..<16, withTermination: .nullAndSpace)
+        #expect(
+            hdr == [97, 98, 99, 100, 101, 102, 0, 103, 104, 105, 32, 106, 107, 108, 0, 32, 255, 255, 255, 255, 255]
+        )
+
+        hdr.writeString("mno", inField: 16..<21, withTermination: .spaceAndNull)
+        #expect(
+            hdr == [97, 98, 99, 100, 101, 102, 0, 103, 104, 105, 32, 106, 107, 108, 0, 32, 109, 110, 111, 32, 0]
+        )
+    }
 }
