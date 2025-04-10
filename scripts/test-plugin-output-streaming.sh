4c6f38d737836009439340106ab169ea920c61d9
Euan Harris <euanh@apple.com>
2025-04-10 13:54:01 +0100

    tests: Improve diagnostics and robustness of test shell scripts (#81)
    Motivation
    ----------
    
    Tests scripts are used by GitHub Actions and when running tests locally.
    They should have consistent structures, clean up after themselves and
    print useful messages so that failures can be diagnosed.
    
    Modifications
    -------------
    
    * Update all test scripts to use the same structure and cleanup options
    as `run-integration-tests.sh`
    * Add better diagnostic messages
    
    Result
    ------
    
    Test failures will be easier to diagnose
    
    Test Plan
    ---------
    
    Existing tests continue to pass.

--- scripts/test-plugin-output-streaming.sh
+++ scripts/test-plugin-output-streaming.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftContainerPlugin open source project
+##
+## Copyright (c) 2025 Apple Inc. and the SwiftContainerPlugin project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftContainerPlugin project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
 
 # Test that error output streamed from containertool is printed correctly by the plugin. 
 
-set -exo pipefail
+set -euo pipefail
 
 log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
@@ -16,12 +29,15 @@ cleanup() {
 }
 trap cleanup EXIT
 
+# Create a test project which depends on this checkout of the plugin repository
+REPO_ROOT=$(git rev-parse --show-toplevel)
 swift package --package-path "$PKGPATH" init --type executable --name hello
 cat >> "$PKGPATH/Package.swift" <<EOF
 package.dependencies += [
-    .package(path: "$PWD"),
+    .package(path: "$REPO_ROOT"),
 ]
 EOF
+cat "$PKGPATH/Package.swift"
 
 # Run the plugin, forgetting a mandatory argument.   Verify that the output is not corrupted.
 # The `swift package` command will return a nonzero exit code.   This is expected, so disable pipefail.
@@ -29,10 +45,11 @@ set +o pipefail
 swift package --package-path "$PKGPATH" --allow-network-connections all build-container-image 2>&1 | tee "$PKGPATH/output"
 set -o pipefail
 
+# This checks that the output lines are not broken, but not that they appear in the correct order
 grep -F -x -e "error: Missing expected argument '--repository <repository>'" \
            -e "error: Help:  --repository <repository>  Repository path" \
            -e "error: Usage: containertool [<options>] --repository <repository> <executable>" \
            -e "error:   See 'containertool --help' for more information." "$PKGPATH/output"
 
-echo Plugin error output: PASSED
+log Plugin error output: PASSED
 
