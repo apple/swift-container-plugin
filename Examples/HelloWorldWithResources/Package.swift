00a23c5f136a3e42b6289af219ae804a96e58352
Euan Harris <euanh@apple.com>
2025-04-11 14:44:21 +0100

    Examples: Add example using bundled resources

---
+++ Examples/HelloWorldWithResources/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftContainerPlugin open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftContainerPlugin project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftContainerPlugin project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+    name: "hello-world",
+    platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.1.0"),
+        .package(url: "https://github.com/apple/swift-container-plugin", from: "0.5.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "hello-world",
+            dependencies: [.product(name: "Hummingbird", package: "hummingbird")],
+            resources: [.process("resources")]
+        )
+    ]
+)
