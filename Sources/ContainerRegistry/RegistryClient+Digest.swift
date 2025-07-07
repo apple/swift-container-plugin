95fa15626d63a84b2065fc0a1ea24d6936332c0b
Euan Harris <euanh@apple.com>
2025-07-07 12:02:36 +0100

    ContainerRegistry: Add sha512 digest

--- Sources/ContainerRegistry/RegistryClient+Digest.swift
+++ Sources/ContainerRegistry/RegistryClient+Digest.swift
@@ -15,16 +15,30 @@
 import Foundation
 import HTTPTypes
 import struct Crypto.SHA256
+import struct Crypto.SHA512
 
 // The spec says that Docker- prefix headers are no longer to be used, but also specifies that the registry digest is returned in this header.
 extension HTTPField.Name { static let dockerContentDigest = Self("Docker-Content-Digest")! }
 
 /// Calculates the digest of a blob of data.
-/// - Parameter data: Blob of data to digest.
+/// - Parameters:
+///   - data: Blob of data to digest.
+///   - algorithm: Digest algorithm to use.
 /// - Returns: The blob's digest, in the format expected by the distribution protocol.
-public func digest(of data: any DataProtocol) -> ImageReference.Digest {
+public func digest(
+    of data: any DataProtocol,
+    algorithm: ImageReference.Digest.Algorithm = .sha256
+) -> ImageReference.Digest {
     // SHA256 is required; some registries might also support SHA512
-    let hash = SHA256.hash(data: data)
-    let digest = hash.compactMap { String(format: "%02x", $0) }.joined()
-    return try! ImageReference.Digest("sha256:" + digest)
+    switch algorithm {
+    case .sha256:
+        let hash = SHA256.hash(data: data)
+        let digest = hash.compactMap { String(format: "%02x", $0) }.joined()
+        return try! ImageReference.Digest("sha256:" + digest)
+
+    case .sha512:
+        let hash = SHA512.hash(data: data)
+        let digest = hash.compactMap { String(format: "%02x", $0) }.joined()
+        return try! ImageReference.Digest("sha512:" + digest)
+    }
 }
